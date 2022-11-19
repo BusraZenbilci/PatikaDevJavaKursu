d25b45f18274bc31aa27725cadd422133aa44974
Büşra Zenbilci <88310614+BusraZenbilci@users.noreply.github.com>
2022-11-19 16:50:44 +0300

    Add files via upload

---
+++ SinifiGecmeDurumu/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int mat, fizik , turkce, kimya, muzik;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Matematik notunuz : ");
+        mat = input.nextInt();
+
+        System.out.println("Türkçe notunuz : ");
+        turkce= input.nextInt();
+
+        System.out.println("Fizik notunuz : ");
+        fizik = input.nextInt();
+
+        System.out.println("Kimya notunuz : ");
+        kimya = input.nextInt();
+
+        System.out.println("Müzik notunuz : ");
+        muzik= input.nextInt();
+
+        double average = (mat + turkce + fizik + kimya + muzik) / 5;
+        if(average <= 55){
+            System.out.println("Sınıfta kaldınız.");
+        }
+        else
+            System.out.println("Sınıfı geçtiniz.");
+
+        System.out.println("Ortalamanız: " + average);
+    }
+}
